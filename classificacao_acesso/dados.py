e3afbab7da5df9ccd5d800445ade48795bd49c7c
Mauricio Rodrigues de Morais <mmorais@gmail.com>
2019-03-15 07:38:38 -0300

    aula 2

---
+++ classificacao_acesso/dados.py
@@ -0,0 +1,26 @@
+import csv
+
+def carregar_acessos():
+    X = []
+    Y = []
+
+    arquivo = open('../download/classificacao/acesso.csv','r')
+    leitor = csv.reader(arquivo)
+    next(leitor)
+
+    for home, como_funciona, contato, comprou in leitor:
+        X.append([int(home), int(como_funciona), int(contato)])
+        Y.append(int(comprou))
+    
+    """ Debug info 
+    print(X)
+    print(len(X))
+    print(Y)
+    print(len(Y))
+    """
+
+    return X, Y
+
+
+carregar_acessos()
+    
